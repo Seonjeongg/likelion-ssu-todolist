b7a716fbd96a7edb5ba98155e003feaaa4cc6fd8
Seonjeongg <132347712+Seonjeongg@users.noreply.github.com>
2024-08-25 15:02:48 +0900

    Update urls.py

--- todoApp/urls.py
+++ todoApp/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("<int:user_id>", views.Todos.as_view())
+    path("<int:user_id>", views.Todos.as_view()),
+    path("<int:user_id>/<int:todo_id>/check", views.CheckTodo.as_view(), name='check_todo'),
+    path("<int:user_id>/<int:todo_id>/reviews", views.ReviewTodo.as_view(), name='review_todo'),
 ]
