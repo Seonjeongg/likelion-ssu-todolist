2cb77c64a186dd524a5dcc7e0cc70e7530ef2688
Seonjeongg <132347712+Seonjeongg@users.noreply.github.com>
2024-08-25 15:10:34 +0900

    Update models.py

--- todoApp/models.py
+++ todoApp/models.py
@@ -3,19 +3,14 @@ from userApp.models import User
 
 class Todo(models.Model):
   """ Todo Model Definition """
-
-  user = models.ForeignKey(User, on_delete=models.CASCADE)
-  date = models.DateTimeField()
-  content = models.TextField()
-  is_checked = models.BooleanField(default=False)
-  review = models.TextField(blank=True, default="")
-  emoji = models.CharField(
-    max_length=1,
-    default="",
-    blank=True
-  )
-  created_at = models.DateTimeField(auto_now_add=True)
-  updated_at = models.DateTimeField(auto_now=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    date = models.DateTimeField()
+    content = models.TextField()
+    is_checked = models.BooleanField(default=False)
+    emoji = models.CharField(max_length=1, default="", blank=True)
+    review = models.TextField(blank=True, default="")  # 리뷰 필드 추가
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
   def __str__(self):
     return self.content
