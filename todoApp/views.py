e87fac305bb6a2a940150618fabe774f4b54b1ea
Seonjeongg <132347712+Seonjeongg@users.noreply.github.com>
2024-08-25 15:31:25 +0900

    Update views.py

--- todoApp/views.py
+++ todoApp/views.py
@@ -4,6 +4,7 @@ from rest_framework.response import Response
 from rest_framework.exceptions import NotFound
 from .models import Todo, User
 from .serializers import TodoSerializer
+from rest_framework import status
 
 class Todos(APIView):
 
